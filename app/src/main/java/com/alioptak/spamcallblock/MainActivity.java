b6276bf96e1bc2256f4a19f84bd6002e58784592
Emile Mongenet <emile.mongenet@hei.yncrea.fr>
2021-02-23 11:37:50 +0100

    Add button to go HistoricActivity

--- app/src/main/java/com/alioptak/spamcallblock/MainActivity.java
+++ app/src/main/java/com/alioptak/spamcallblock/MainActivity.java
@@ -10,12 +10,20 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button button_main_gocontact;
+    Button button_main_gohistoric;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_main_gohistoric = findViewById(R.id.button_main_gohistoric);
+        button_main_gohistoric.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
+                goToHistoric();
+            }
+        });
         button_main_gocontact = findViewById(R.id.button_main_gocontact);
         button_main_gocontact.setOnClickListener(new View.OnClickListener(){
 
@@ -31,5 +39,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void goToHistoric(){
+        Intent i = new Intent(this, HistoricActivity.class);
+        startActivity(i);
+    }
 
 }
