eaf8dd2ff9a8979d8a2c8f3e1173a6126c22cc1e
AlioptK <Hupode)6>
2021-02-23 08:41:40 +0100

    Slight error Singleton

--- app/src/main/java/com/alioptak/spamcallblock/MainActivity.java
+++ app/src/main/java/com/alioptak/spamcallblock/MainActivity.java
@@ -10,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Pour emile
-        setContentView(R.layout.activity_main);
-
     }
 }
